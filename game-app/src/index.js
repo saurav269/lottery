9478ce53bdd8126407fe6f8b483e1b22e7f50170
Saurav Mallik <sauravmallik786@gmail.com>
2023-12-18 10:14:20 +0530

    huu

--- game-app/src/index.js
+++ game-app/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
+import { HashRouter} from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
   <AuthProvider>
+  <HashRouter>
     <App />
+    </HashRouter>
     </AuthProvider>
-    </BrowserRouter>
   
 );
 
